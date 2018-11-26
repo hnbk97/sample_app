c310316dce06a90d16a6fa248c524df637f42bca
hnbk97 <hoangkim1997@gmail.com>
2018-11-26 09:14:57 +0700

    Finish chapter8

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,15 @@ class User < ApplicationRecord
   validates :password, presence: true,
   length: {minimum: Settings.user_validates.password_min}
 
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost: cost
+  end
+
   def downcase_email
     email.downcase!
   end
