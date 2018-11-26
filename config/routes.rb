5a573dba88c838fe583b45134aa5b24a7485a5a9
hnbk97 <hoangkim1997@gmail.com>
2018-11-26 09:14:57 +0700

    Finish chapter5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|vi/ do
-    root "static_pages#home"
-    get "help" => "static_pages#help"
-    get "about" => "static_pages#about"
-  end
+  root "static_pages#home"
+  get  "/help", to: "static_pages#help"
+  get  "/about", to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
 end
